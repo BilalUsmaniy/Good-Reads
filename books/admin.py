43e964f7b175c9f16d6c558d3ed8cd23616bae4d
BilalUsmaniy <83291180+BilalUsmaniy@users.noreply.github.com>
2022-11-02 04:33:17 -0400

    Created Admin panel, optimized admin panel
    Created superuser. Optimized admin panel,

--- books/admin.py
+++ books/admin.py
@@ -9,12 +9,15 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'isbn', 'description')
 
 class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('first_name', 'last_name', 'email')
     list_display = ('first_name', 'last_name', 'email', 'bio')
 
 class BookAuthorAdmin(admin.ModelAdmin):
+    search_fields = ('author', 'book')
     list_display = ('author', 'book')
 
 class BookReviewAdmin(admin.ModelAdmin):
+    search_fields = ('user', 'stars_given', 'book')
     list_display = ('user', 'stars_given', 'book')
 
 
